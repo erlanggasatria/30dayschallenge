fb1997ae38893aa0fbe3351d65af7540988ff0c5
I Dewa Gde Satria Pramana Erlangga <satriapramana56@gmail.com>
2023-11-20 13:39:07 +0800

    Login With React

--- 18.-login-react/src/components/LoginSignup/LoginSignup.jsx
+++ 18.-login-react/src/components/LoginSignup/LoginSignup.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import './LoginSignup.css'
+import React, { useState } from "react";
+import './LoginSignup.css';
+
 
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
+
+    const[action,setAction] = useState("Sign Up");
+
     return (
         <div>
             <div className="countainer">
                 <div className="header">
-                    <div className="text">Sign Up</div>
+                    <div className="text">{action}</div>
                     <div className="underline"></div>
                 </div>
             <div className="input">
+                {action==="Login"?<div></div>:                
                 <div className="inputs">
                     <img src={user_icon} alt="" />
                     <input type="text" placeholder="username" />
-                </div>
-                </div>
+                </div>}
+        
                 <div className="inputs">
                     <img src={email_icon} alt="" />
                     <input type="email" placeholder="email" />
@@ -29,11 +34,12 @@ const LoginSignup = () => {
                 </div>  
                 <div className="forgot-password"><span>Lost Password?</span></div>
                 <div className="submit-countainer">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={()=>{setAction('Sign Up')}}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={()=>{setAction('Login')}}>Login</div>
             </div>
             </div>
         </div>
+    </div>
     )
 }
 
